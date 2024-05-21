ee8daf0999e163c34cc7879e15ed0e3234682ad0
Rohit Kumar Singh <kumarsingh.rohit7@gmail.com>
2024-05-22 00:40:54 +0530

    Update mongocheck.py

--- mongocheck.py
+++ mongocheck.py
@@ -2,8 +2,24 @@
 
 import streamlit as st
 import pymongo
+from pymongo.mongo_client import MongoClient
 
 st.write("Hello")
-st.write(st.secrets["uri"])
-print(st.secrets["uri"])
+uri = st.secrets["uri"])
 
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+    db = client.neuraldb
+    people = db.people
+    people.insert_one({"name" : "Mike", "age" : 30}) 
+    people.insert_one({"name" : "Lisa", "age" : 20, "interestes" : ["C++", "Python"]}) 
+
+    for person in people.find():
+        print(person)
+except Exception as e:
+    print(e)
